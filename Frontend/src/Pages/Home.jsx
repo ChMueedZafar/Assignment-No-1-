7dac92e239ff52cd9d4dd51c7ee7e582ba21ed7d
chmueedzafar <chmueedzafar@gmail.com>
2024-12-12 19:10:21 +0500

    second last commit

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-
+import HeroSection from '../Components/HeroSection'
+import About from '../Components/About'
+import Qualities from '../Components/Qualities'
+import Menu from '../Components/Menu'
+import WhoAreWe from '../Components/WhoAreWe'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <HeroSection />
+    <About />
+    <Qualities />
+    <Menu />
+    <WhoAreWe />
+    </>
   )
 }
 
-export default Home
+export default Home;
