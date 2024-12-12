a440f630f84584909767bce3411fa256a10c9392
chmueedzafar <chmueedzafar@gmail.com>
2024-12-12 21:25:12 +0500

    last commit

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,6 +5,9 @@ import About from '../Components/About'
 import Qualities from '../Components/Qualities'
 import Menu from '../Components/Menu'
 import WhoAreWe from '../Components/WhoAreWe'
+import Team from '../Components/Team'
+import Reservation from '../Components/Reservation'
+import Footer from '../Components/Footer'
 const Home = () => {
   return (
     <>
@@ -13,6 +16,9 @@ const Home = () => {
     <Qualities />
     <Menu />
     <WhoAreWe />
+    <Team />
+    <Reservation />
+    <Footer/>
     </>
   )
 }
